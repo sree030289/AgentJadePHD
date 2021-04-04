a39407288dbb92d6d49f994213d4f3d7c88310ae
sreeramvenapusa <sreeramvenapusa@nbnco.com.au>
2021-04-04 12:50:07 +0530

    Commiting 1st version of Jade microgrid

--- src/org/MicroGridJade/MainContainerAgent.java
+++ src/org/MicroGridJade/MainContainerAgent.java
@@ -10,6 +10,15 @@ public class MainContainerAgent {
 
 
         jade.Boot.main(args1);
+//        String [] args1 ={"-gui"};
+////        String [] args2 ={"-container","BatteryGrid1:org.grid1.BatteryAgentFirstGrid;SolarGrid1:org.grid1.SolarAgentFirstGrid;LoadGrid1:org.grid1.LoadAgentFirstGrid;WindGrid1:org.grid1.WindAgentFirstGrid"};
+////        String [] args3 ={"-container","SolarGrid2:org.grid2.SolarAgentSecondGrid;LoadGrid2:org.grid2.LoadAgentSecondGrid;WindGrid2:org.grid2.WindAgentSecondGrid"};
+////        String [] args4 ={"-container","SolarGrid3:org.grid3.SolarAgentThirdGrid;LoadGrid3:org.grid3.LoadAgentThirdGrid;WindGrid3:org.grid3.WindAgentThirdGrid"};
+//
+//        jade.Boot.main(args1);
+        //jade.Boot.main(args2);
+//        jade.Boot.main(args3);
+//        jade.Boot.main(args4);
 
     }
 
